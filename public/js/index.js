5f321c77454543cd939d35a4477ae4c39306eb92
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-05-04 15:37:24 -0400

    change menu layout for mobile

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,7 @@
 //Allows link to specific accordion tab
 $(document).ready(function () {
   location.hash && $(location.hash + '.collapse').collapse('show');
+  $(".nav-item .dropdown-item").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
 });

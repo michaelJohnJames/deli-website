034eb4afb30b239419e18d95e504916c50e9bd59
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-05-04 16:19:02 -0400

    comment html and format

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,13 @@
 //Allows link to specific accordion tab
-$(document).ready(function () {
+$(document).ready(function() {
   location.hash && $(location.hash + '.collapse').collapse('show');
 
+//Makes dropdown disappear when clicked on link
   $(".nav-item").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });
 
+//Makes dropdown disappear when clicking on dropdown item
   $(".dropdown-item").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });

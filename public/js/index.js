102385d4cc53cb783d91fd29732dbd7d08b784f7
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-05-04 16:03:17 -0400

    style dropdown list

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,12 @@
 //Allows link to specific accordion tab
 $(document).ready(function () {
   location.hash && $(location.hash + '.collapse').collapse('show');
-  $(".nav-item .dropdown-item").click(function(event) {
+
+  $(".nav-item").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+
+  $(".dropdown-item").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });
 });
